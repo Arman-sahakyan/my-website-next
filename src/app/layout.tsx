048e931828b63fd72119934f2b8c3996a0ef6288
Arman-sahakyan <armansahakyan411@gmail.com>
2025-10-23 23:49:47 +0400

    admin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "../styles/hero-permit.css";
 import "../styles/tokens.css";
-import "../styles/global.css"
+import "../styles/global.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,13 +19,15 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Oregon Trucking Permit",
-  description: "Apply for Oregon trucking permits easily with our private online service.",
+  description:
+    "Apply for Oregon trucking permits easily with our private online service.",
   icons: {
     icon: "/logo.png",
     shortcut: "/logo.png",
     apple: "/logo.png",
   },
-  keywords: 'Oregon trucking, oversize permit, truck permit, heavy haul, oregon truck permit, oregon trucking permit',
+  keywords:
+    "Oregon trucking, oversize permit, truck permit, heavy haul, oregon truck permit, oregon trucking permit",
 };
 
 export default function RootLayout({
@@ -32,13 +35,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    name: "Oregon Trucking Permit",
+    url: "https://www.oregontruckingpermit.com",
+    logo: "https://www.oregontruckingpermit.com/logo.png",
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+15034387888",
+      contactType: "Customer Service",
+      areaServed: "US",
+    },
+    
+  };
+
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
         <Footer />
-        
       </body>
     </html>
   );
